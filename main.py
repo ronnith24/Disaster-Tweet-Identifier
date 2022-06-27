f36f305c3ee7c8260c0ccca401d17c89375842d8
Ronnith Nandy <ronnith.nandy@gmail.com>
2022-06-27 17:06:33 +0530

    imported modules and loaded the data

---
+++ main.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+# Loading Data START
+train_df = pd.read_csv("data/train.csv")
+test_df = pd.read_csv("data/test.csv") 
+# Loading Data END
+
+
+## MODEL START
+
+# model definition
+
+# model train
+
+## MODEL END
+
+
+
+# predicts whether the string tweet represents a disaster or not using the trained model
+def predict(tweet):
+    pass
+
+# uses the predict(str) function to create the submission
+def create_submission():
+    pass
+
+
